1094f125bcb874f4d8b43d6840940c6143451184
armutyus <ofdelibas@gmail.com>
2023-11-27 17:17:47 +0300

    signup auth.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
-console.log("Hello, World");
 // IMPORTS FROM PACKAGES
 const express = require("express");
+const mongoose = require("mongoose");
+
+// IMPORTS FROM OTHER FILES
+const authRouter = require("./routes/auth");
 
 // INIT
 const PORT = 3000;
 const app = express();
+const DB = "mongodb+srv://armutyus:armut1991@cluster0.qszek4b.mongodb.net/?retryWrites=true&w=majority"
+
+// middleware
+app.use(express.json());
+app.use(authRouter);
+
+// Connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`connected at port ${PORT}`);
