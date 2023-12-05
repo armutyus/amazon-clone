91cb72c0628760f0c7d2776934916c26168a91e7
armutyus <ofdelibas@gmail.com>
2023-12-05 12:50:39 +0300

    token validation and bottom bar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:amazon_clone_project/constants/global_variables.dart';
+import 'package:amazon_clone_project/feature/auth/services/auth_services.dart';
 import 'package:amazon_clone_project/providers/user_provider.dart';
 import 'package:amazon_clone_project/router.dart';
+import 'package:amazon_clone_project/widgets/bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,8 +14,21 @@ void main() {
   ], child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthService authService = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+    authService.getData(context: context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +47,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       onGenerateRoute: (settings) => generateRoute(settings),
-      home: const AuthScreen(),
+      home: Provider.of<UserProvider>(context).user.token.isNotEmpty ? const BottomBar() : const AuthScreen(),
     );
   }
 }
